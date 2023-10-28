07f041b6bf19f9be197d7a9a8e874d860a036b2e
Vivek <vivekbonde15l@gmail.com>
2023-10-28 11:51:51 +0530

    add

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def delete(message_id):
     return redirect(url_for('hello'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
